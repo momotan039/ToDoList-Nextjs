36a626630b24e7d1f78082252deecd59d7289b6e
momotan <momotaha039@gmail.com>
2024-05-15 00:47:40 +0300

    refactor home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
-import Header from "@/components/Header";
-import Image from "next/image";
+import Card from "@/components/Card";
 
 export default function Home() {
   return (
-   <main>
-    <Header/>
-   </main>
+    <div className="flex justify-center mt-12">
+      <Card>
+      <h1 className="text-white font-bold text-center text-4xl">
+        This is First Nextjs App
+      </h1>
+      <p className="text-center text-blue-300 font-bold mt-6">This app crafted using Typescript,TailwindCss</p>
+    </Card>
+    </div>
   );
 }
