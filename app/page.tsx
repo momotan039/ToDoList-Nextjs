601da2ce005254eb25f8aa85d29ac5083703aec3
momotan <momotaha039@gmail.com>
2024-05-16 17:38:48 +0300

    refactor main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 import Card from "@/components/Card";
+import Todos from "@/components/Todos";
 
 export default function Home() {
   return (
-    <div className="flex justify-center mt-12">
-      <Card>
-      <h1 className="text-white font-bold text-center text-4xl">
+    <div className="flex justify-center items-center flex-col h-screen">
+      <h1 className="text-white font-bold text-center text-6xl">
         This is First Nextjs App
       </h1>
-      <p className="text-center text-blue-100 font-bold mt-6">This app crafted using Typescript,TailwindCss</p>
-    </Card>
+      <p className="text-center text-blue-100 font-bold mt-6 text-xl">This app crafted using Typescript,TailwindCss</p>
     </div>
   );
 }
