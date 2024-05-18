54dff41a0bd2337985b53a29bdd33093200083e0
momotan <momotaha039@gmail.com>
2024-05-18 14:42:01 +0300

    add todos to the main context

--- context/appContext.tsx
+++ context/appContext.tsx
@@ -1,19 +1,28 @@
 'use client'
+import { ToDoType } from '@/components/Todo';
 import { ScriptProps } from 'next/script';
 import React, { useState } from 'react'
 
-interface appContextType{
-    setAppState:any,
-    name?:string
+export interface appContextType{
+    setAppState?:any,
+    todos:ToDoType[]
 }
-const AppContext=React.createContext<appContextType>({});
+const AppContext=React.createContext<appContextType>({todos:[]});
 
 export function useAppContext(){
     return React.useContext(AppContext)
 }
 
 function AppContextProvider({children}:ScriptProps) {
-  const [appState, setAppState] = useState({name:'mohammed'})
+  const data=[
+    {id:1,done:false,title:'This is 1 ToDo',description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, magni.'},
+    {id:2,done:false,title:'This is 2 ToDo',description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, magni.'},
+    {id:3,done:true,title:'This is 3 ToDo',description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, magni.'},
+    {id:4,done:false,title:'This is 4 ToDo',description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, magni.'},
+    {id:5,done:false,title:'This is 5 ToDo',description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, magni.'},
+    {id:6,done:false,title:'This is 6 ToDo',description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, magni.'},
+]
+  const [appState, setAppState] = useState({todos:data})
   return (
     <AppContext.Provider value={{...appState,setAppState:setAppState}}>
         {children}
