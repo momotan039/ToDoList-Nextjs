86e69403c939f713ba82313d36db53b044b0e7c3
momotan <momotaha039@gmail.com>
2024-05-18 21:41:39 +0300

    use container comopnent and fix style

--- components/Todos.tsx
+++ components/Todos.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import Todo, { ToDoType } from './Todo'
+import Container from './Container'
 
 
 function Todos({ todos }: { todos: Array<ToDoType> }) {
   const doneTasks = todos.filter(f => f.done)
   const inProgoresTasks = todos.filter(f => !f.done)
   return (
-    <>
+    <Container>
+      <>
       {
         inProgoresTasks.length>0? <>
           <h1 className='divide-red-50 text-white font-bold text-2xl divide-y-2 divide-white'>In Progress Tasks </h1>
-          <div className="bg-gray-800 flex flex-wrap">
+          <div className="bg-gray-800 flex flex-wrap mb-4">
             {todos.filter(f=>!f.done).map((t, i) => {
               return <Todo key={t.id} task={t} />
             })}
@@ -31,6 +33,7 @@ function Todos({ todos }: { todos: Array<ToDoType> }) {
         :null
       }
     </>
+    </Container>
   )
 }
 
