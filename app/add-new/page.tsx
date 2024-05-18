c9350dc624e930a7db5727e6854a72eaa82745e8
momotan <momotaha039@gmail.com>
2024-05-18 19:10:55 +0300

    fix row prop in textarea

--- app/add-new/page.tsx
+++ app/add-new/page.tsx
@@ -4,7 +4,7 @@ import { ToDoType } from '@/components/Todo'
 import { appContextType, useAppContext } from '@/context/appContext'
 import { useState,useEffect } from 'react'
 
-function page() {
+function AddNewTask() {
   const app=useAppContext()
   const [task, setTask] = useState<ToDoType>({title:'',done:false,description:''})
   const createTodo=()=>{
@@ -22,11 +22,11 @@ function page() {
           ➕Add New To Do
         </h1>
         <input value={task.title} onChange={(e)=>setTask({...task,title:e.target.value})} type="text" className='rounded px-1 py-1 text-blue-950 font-bold w-full' placeholder='Enter new Todo' />
-        <textarea value={task.description} onChange={(e)=>setTask({...task,description:e.target.value})} rows='8' placeholder='Enter here a description' className=' rounded mt-3 px-1 text-blue-950 font-bold w-full'></textarea>
+        <textarea value={task.description} onChange={(e)=>setTask({...task,description:e.target.value})} rows={8} placeholder='Enter here a description' className=' rounded mt-3 px-1 text-blue-950 font-bold w-full'></textarea>
           <button onClick={createTodo} className='py-3 mt-3 w-1/3 font-bold text-white text-xl bg-slate-700 rounded transition-all hover:bg-slate-800'>Add</button>
       </Card>
     </div>
   )
 }
 
-export default page
+export default AddNewTask
