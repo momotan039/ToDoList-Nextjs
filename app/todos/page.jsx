920ef3501831ea019795e2016dfda60eef165187
momotan <momotaha039@gmail.com>
2024-05-18 14:43:57 +0300

    get todos from app context

--- app/todos/page.jsx
+++ app/todos/page.jsx
@@ -4,22 +4,10 @@ import Todos from '@/components/Todos'
 import { useAppContext } from '@/context/appContext'
 
 function page() {
-    const todos=[
-        {title:'This is 1 ToDo',description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, magni.'},
-        {title:'This is 2 ToDo',description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, magni.'},
-        {title:'This is 3 ToDo',description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, magni.'},
-        {title:'This is 4 ToDo',description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, magni.'},
-        {title:'This is 5 ToDo',description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, magni.'},
-        {title:'This is 6 ToDo',description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, magni.'},
-        {title:'This is 7 ToDo',description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, magni.'},
-        {title:'This is 8 ToDo',description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, magni.'},
-        {title:'This is 9 ToDo',description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, magni.'},
-    ]
-   const ctx=useAppContext()
-   console.log(ctx);
+   const appState=useAppContext()
   return (
     <div className="m-5">
-    <Todos todos={todos}/>
+    <Todos todos={appState.todos}/>
   </div>
   )
 }
