764965c59af7c8ac7064897306e2a8ed004770a3
momotan <momotaha039@gmail.com>
2024-05-18 21:42:32 +0300

    restyle Todo Component and fix responsivity

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -24,10 +24,10 @@ function Todo({ task }: ToDoProps) {
     app.setAppState((s:appContextType)=>({...s,todos:app.todos.filter(f=>f.id!=task.id)}))
   }
   return (
-    <div className="transition duration-300  p-2 w-1/4 relative">
-      <Card bg={`shadow-md shadow-red-100/70 bg-gradient-to-r ${task.done ? 'from-teal-700 ' : 'from-blue-700'}`} padding='py-4 px-3' height='h-36'>
+    <div className="transition duration-300  p-2 w-full  md:w-2/4 xl:w-1/4 relative ">
+      <Card bg={`shadow-md shadow-red-100/70 bg-gradient-to-r ${task.done ? 'from-teal-700 ' : 'from-blue-700'}`} padding='py-4 px-3'>
         <input title='check'  checked={task.done} onChange={changeStatus} type="checkbox" className='absolute top-6 right-24 w-6 h-6 cursor-pointer peer/check' />
-        <label  className="peer-checked/check:text-amber-400/50 peer-checked/check:font-bold peer-checked/check:block hidden">Done</label>
+        <label  className="peer-checked/check:text-amber-400 peer-checked/check:font-bold peer-checked/check:block text-amber-400/50">Done</label>
         <button title='Remove' onClick={deltedTask} className='absolute top-6 right-6 w-6 h-6 bg-white'>❌</button>
         <Link title='Edit' className='absolute top-6 right-14 w-6 h-6 bg-white' href={'/edit-todo/'+task.id}>📝</Link>
         <h1 className={`text-white   font-bold ${task.done ? 'line-through' : ''}  text-3xl `}>{task.title}</h1>
