faeedd6b6038c7244ec8084995af24543e90e4cd
momotan <momotaha039@gmail.com>
2024-05-18 21:40:40 +0300

    restyle container and fix responsive

--- components/Container.tsx
+++ components/Container.tsx
@@ -3,7 +3,7 @@ import React, { ReactNode } from 'react'
 
 function Container({children}:ScriptProps) {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto transition-all ease-in-out duration-300 px-3 sm:px-0">
         {children}
     </div>
   )
