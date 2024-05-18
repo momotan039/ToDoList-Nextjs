0a880fca03d78db853c797a965b9b11f8989d3c7
momotan <momotaha039@gmail.com>
2024-05-18 22:11:01 +0300

    add lucide to menetioned techs

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <h1 className="text-white font-bold text-center text-3xl sm:text-4xl   lg:text-6xl ">
         This is First Nextjs App
       </h1>
-      <p className="text-center text-blue-100 font-bold mt-6 text-xl">This app crafted using Typescript,TailwindCss</p>
+      <p className="text-center text-blue-100 font-bold mt-6 text-xl">This app crafted using Typescript,TailwindCss,Lucide</p>
     </div>
   );
 }
