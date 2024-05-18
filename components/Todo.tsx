ad42ffa82bb58f153204df9bc186b41da3cacab6
momotan <momotaha039@gmail.com>
2024-05-18 19:10:09 +0300

    add edit button and change style

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import Card from './Card'
 import { appContextType, useAppContext } from '@/context/appContext'
+import Link from 'next/link'
 
 export interface ToDoType {
   id?: number,
@@ -25,9 +26,10 @@ function Todo({ task }: ToDoProps) {
   return (
     <div className="transition duration-300  p-2 w-1/4 relative">
       <Card bg={`shadow-md shadow-red-100/70 bg-gradient-to-r ${task.done ? 'from-teal-700 ' : 'from-blue-700'}`} padding='py-4 px-3' height='h-36'>
-        <input  checked={task.done} onChange={changeStatus} type="checkbox" className='absolute top-6 right-14 w-6 h-6 cursor-pointer peer/check' />
-        <label  className="peer-checked/check:text-orange-400|font-bold">Done</label>
-        <button onClick={deltedTask} className='absolute top-6 right-6 w-6 h-6 bg-white'>❌</button>
+        <input title='check'  checked={task.done} onChange={changeStatus} type="checkbox" className='absolute top-6 right-24 w-6 h-6 cursor-pointer peer/check' />
+        <label  className="peer-checked/check:text-amber-400/50 peer-checked/check:font-bold peer-checked/check:block hidden">Done</label>
+        <button title='Remove' onClick={deltedTask} className='absolute top-6 right-6 w-6 h-6 bg-white'>❌</button>
+        <Link title='Edit' className='absolute top-6 right-14 w-6 h-6 bg-white' href={'/edit-todo/'+task.id}>📝</Link>
         <h1 className={`text-white   font-bold ${task.done ? 'line-through' : ''}  text-3xl `}>{task.title}</h1>
         <p className="text-amber-300 font-bold mt-4">{task.description}</p>
       </Card>
