3ccb75ccea2c09b6a9bad72169c3ca26d77cbaf5
momotan <momotaha039@gmail.com>
2024-05-18 21:41:06 +0300

    restyle home page and fix responsive

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Todos from "@/components/Todos";
 export default function Home() {
   return (
     <div className="flex justify-center items-center flex-col h-screen">
-      <h1 className="text-white font-bold text-center text-6xl">
+      <h1 className="text-white font-bold text-center text-3xl sm:text-4xl   lg:text-6xl ">
         This is First Nextjs App
       </h1>
       <p className="text-center text-blue-100 font-bold mt-6 text-xl">This app crafted using Typescript,TailwindCss</p>
