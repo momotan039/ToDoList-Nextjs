e32301a187fd79c768ef193167a035b6e9bbe52b
momotan <momotaha039@gmail.com>
2024-05-18 21:40:10 +0300

    create constant var for links

---
+++ constants/nav-links.ts
@@ -0,0 +1,14 @@
+interface navLinksType{
+    href:string,
+    name:string
+}
+export const navLinks:navLinksType[]=[
+    {
+        href:'/todos',
+        name:'Todos'
+    },
+    {
+        href:'/add-new',
+        name:'Add ToDo'
+    },
+]
