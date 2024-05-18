62e1e0fdbc886698d6dc33da3be03c057d5e31d0
momotan <momotaha039@gmail.com>
2024-05-18 22:06:15 +0300

    use  icons from lucide

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Card from './Card'
 import { appContextType, useAppContext } from '@/context/appContext'
 import Link from 'next/link'
+import {X,Edit,SquareCheckBig,Square} from 'lucide-react'
 
 export interface ToDoType {
   id?: number,
@@ -26,11 +27,10 @@ function Todo({ task }: ToDoProps) {
   return (
     <div className="transition duration-300  p-2 w-full  md:w-2/4 xl:w-1/4 relative ">
       <Card bg={`shadow-md shadow-red-100/70 bg-gradient-to-r ${task.done ? 'from-teal-700 ' : 'from-blue-700'}`} padding='py-4 px-3'>
-        <input title='check'  checked={task.done} onChange={changeStatus} type="checkbox" className='absolute top-6 right-24 w-6 h-6 cursor-pointer peer/check' />
-        <label  className="peer-checked/check:text-amber-400 peer-checked/check:font-bold peer-checked/check:block text-amber-400/50">Done</label>
-        <button title='Remove' onClick={deltedTask} className='absolute top-6 right-6 w-6 h-6 bg-white'>❌</button>
-        <Link title='Edit' className='absolute top-6 right-14 w-6 h-6 bg-white' href={'/edit-todo/'+task.id}>📝</Link>
-        <h1 className={`text-white   font-bold ${task.done ? 'line-through' : ''}  text-3xl `}>{task.title}</h1>
+        <button title='Remove' onClick={deltedTask} className='absolute top-6 right-6 w-6 h-6 bg-white'><X color='red'/></button>
+        <Link title='Edit' className='absolute top-6 right-14 w-6 h-6 bg-white' href={'/edit-todo/'+task.id}><Edit color='black'/></Link>
+        <button title='Check' onClick={changeStatus} className='absolute top-6 left-4 w-6 h-6 bg-white'>{task.done?<SquareCheckBig color='#123123'/>:<Square/>}</button>
+        <h1 className={`text-white mt-6  font-bold ${task.done ? 'line-through' : ''}  text-3xl `}>{task.title}</h1>
         <p className="text-amber-300 font-bold mt-4">{task.description}</p>
       </Card>
     </div>
